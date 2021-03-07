10bfaaa07e6bfd994c95f7320f12e0cacdde2956
Tamara Organ <tamara@organ.dev.br>
2021-03-07 11:16:56 -0400

    Update - Page SignIn

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Image } from 'react-native'
 
-import { Container } from './styles'
+import logoImg from '../../assets/logo.png'
+
+import { Container, Title } from './styles'
 
 const SignIn: React.FC = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  )
 }
 export default SignIn
