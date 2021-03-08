9ff276da4402eb8b41968d126058ed30eb57c8bd
Tamara Organ <tamara@organ.dev.br>
2021-03-08 15:49:51 -0400

    Update - Mobile

---
+++ mobile/src/components/Input/index.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useRef, forwardRef} from 'react';
+import {TextInputProps} from 'react-native';
+import {useField} from '@unform/core';
+
+import {Container, TextInput, Icon} from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+}
+
+interface InputValueReference {
+  value: string;
+}
+
+const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
+  const {registerField, defaultValue = '', fieldName, error} = useField(name);
+  const inputValueRef = useRef<InputValueReference>({value: 'defaultValue'});
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, {useRef, useCallback} from 'react';
+import {
+  Image,
+  ScrollView,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
 
-import { Container } from './styles'
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+
+import {Container, Title, BackToSignIn, BackToSignInText} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container></Container>
-}
-export default SignUp
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  // const handleSignUp = useCallback(async (data: object) => {
+  //   console.log(data);
+  // }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{flex: 1}}>
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={() => {}}>
+              <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#f4ede8" />
+        <BackToSignInText>Voltar para o logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
+};
+
+export default SignUp;
